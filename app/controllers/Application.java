7031995430bff2f373c6e31600ff5b6963f544e9
mr-soze <s_brae@yahoo.com.au>
2016-01-26 10:28:22 +1100

    routed a view using hash maps

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -22,5 +22,20 @@ public class Application extends Controller {
     public Result helloRedirect() {
     	return redirect (controllers.routes.Application.hello()); 
     }
+
+    private static final java.util.Map<Integer, String> productMap = 
+    	new java.util.HashMap<Integer, String>();
+
+    	static {
+    		productMap.put(1, "Keyboard");
+    		productMap.put(2, "Mouse");
+    		productMap.put(3, "Monitor");
+    	}
+
+    public Result listProducts() {
+    	return ok(products.render(productMap.values()));
+    }
+
 }
+
 
